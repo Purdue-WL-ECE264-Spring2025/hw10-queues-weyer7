58d9f7e75c166d3f5e6e4a4772064589efac4356
Alexander Joseph Weyer <weyer7@ececomp2.ecn.purdue.edu>
2025-04-04 15:59:11 -0400

    finished linked_list.c, working on queue.c

--- linked_list.c
+++ linked_list.c
@@ -4,15 +4,71 @@
 
 struct list_node *new_node(size_t value) { return NULL; }
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node *new_node = (struct list_node *)malloc(sizeof(struct list_node));
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+  new_node->value = value; //assign the value to the new node
+  new_node->next = list->head; //point the new node's next to the current head
+  list->head = new_node; //update the list's head to point to the new node
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node *new_node = (struct list_node *)malloc(sizeof(struct list_node));
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+  new_node->value = value;
+  new_node->next = NULL;
 
-void free_list(struct linked_list list) {}
+  if(!list->head) { //case for when list is empty
+    list->head = new_node;
+  }
+  struct list_node *current = list->head;
+  while (current->next) {
+    current = current->next;
+  }
+  current->next = new_node;
+}
+
+size_t remove_from_head(struct linked_list *list) { 
+  if (!list->head) {
+    return 0; //empty list
+  }
+  struct list_node *temp = list->head;
+  // size_t value = temp->value;
+  list->head = temp->next;
+  free(temp);
+  return 0; 
+}
+
+size_t remove_from_tail(struct linked_list *list) { 
+  if (!list->head) {
+    return 0; //empty list
+  }
+  struct list_node *current = list->head;
+  struct list_node *prev = NULL;
+  while (current->next) {
+    prev = current;
+    current = current->next;
+  }
+  // size_t value = current->value;
+  free(current);
+
+  if (prev) {
+    prev->next = NULL;
+  } else {
+    list->head = NULL; //if only one node
+  }
+  return 0; 
+}
+
+void free_list(struct linked_list list) {
+  struct list_node *current = list.head;
+  while (current) {
+    struct list_node *temp = current;
+    current = current->next;
+    free(temp);
+  }
+  list.head = NULL;
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,74 @@
 #include "queue.h"
 #include "tile_game.h"
+#include <stdlib.h>
+#include <stdbool.h>
 
-void enqueue(struct queue *q, struct game_state state) {}
+#define MAX_SEEN_STATES 10461394944
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+void enqueue(struct queue *q, struct game_state state) {
+    size_t value = serialize(state);
+    insert_at_tail(&q->data, value);
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+struct game_state dequeue(struct queue *q) { 
+    size_t value = remove_from_head(&q->data);
+    return (struct game_state){0}; 
+}
+
+int number_of_moves(struct game_state start) { 
+    if (is_solved(start)) {
+        return 0;
+    }
+
+    bool *seen = calloc(MAX_SEEN_STATES, sizeof(bool));
+    if (!seen) {
+        return -1;  // Memory allocation failure
+    }
+
+    struct node {
+        struct game_state state;
+        struct node *next;
+    };
+
+    struct node *queue_front = malloc(sizeof(struct node));
+    queue_front->state = start;
+    queue_front->next = NULL;
+    struct node *queue_rear = queue_front;
+
+    while (queue_front) {
+        struct game_state cur = queue_front->state;
+
+        struct node *temp = queue_front;
+        queue_front = queue_front->next;
+        free(temp);
+
+        struct game_state children[4];
+        int n = valid_moves(cur, children);
+
+        for (int i = 0; i < n; i++) {
+            uint64_t hash = serialize(children[i]) % MAX_SEEN_STATES;
+            if (!seen[hash]) {
+                seen[hash] = true;
+
+                if (is_solved(children[i])) {
+                    free(seen);
+                    while (queue_front) {
+                        struct node *temp = queue_front;
+                        queue_front = queue_front->next;
+                        free(temp);
+                    }
+                    return children[i].num_steps;
+                }
+
+                struct node *new_node = malloc(sizeof(struct node));
+                new_node->state = children[i];
+                new_node->next = NULL;
+                queue_rear->next = new_node;
+                queue_rear = new_node;
+            }
+        }
+    }
+
+    free(seen);
+    return -1;
+}
